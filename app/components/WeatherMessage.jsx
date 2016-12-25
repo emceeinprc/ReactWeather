eaef084ad33362cdaccab7dbe6bdb65b9106e410
emceeinprc <matt@seekpanda.com>
2016-12-25 12:06:41 -0700

    Added modal

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -6,7 +6,7 @@ var WeatherMessage = React.createClass({
         var {temp, location} = this.props;
 
         return (
-            <h3>It is {temp} in {location}.</h3>
+            <h3 className="text-center">It is {temp} in {location}.</h3>
         )
     }
 });
